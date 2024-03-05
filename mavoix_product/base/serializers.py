108838a881581c444b60ed118ce929d49617fefc
Rishabh Tiwari <85389334+rishabh422tiwari@users.noreply.github.com>
2024-03-06 03:15:14 +0530

    Delete mavoix_product directory

--- mavoix_product/base/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['prod_id', 'serial_no', 'catagory', 'family', 'image', 'short_desc', \
-                  'long_desc', 'specification', 'stock', 'batch_no', 'certification']
--- mavoix_product/base/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-from .serializers import ProductSerializer
-from rest_framework.generics import ListAPIView
-
-# Create your views here.
-class ProductView(ListAPIView):
-    
