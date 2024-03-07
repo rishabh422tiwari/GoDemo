8371a97f3e0db3abe46dbca7735c3b29a9eac594
Rishabh Tiwari <rishabh422tiwari@gmail.com>
2024-03-07 05:31:00 +0530

    Integers 7 March

---
+++ GODEMO/helloworld/hello_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	// t.Run("saying hello to people", func(t *testing.T) {
+	// 	got := Hello("Chris")
+	// 	want := "Helloo, Chris"
+
+	// 	asserCorrectMessage(t, got, want)
+
+	// })
+	// t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+	// 	got := Hello("")
+	// 	want := "Hello, World"
+
+	// 	asserCorrectMessage(t, got, want)
+	// })
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		asserCorrectMessage(t, got, want)
+	})
+
+}
+
+func asserCorrectMessage(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+}
